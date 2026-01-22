ee1123dae82f74a9848a3d05465e06b01cdca0c6
Sarvesh <femfitwears@gmail.com>
2026-01-23 00:58:56 +0530

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://YOURDOMAIN.com/",
+      url: "https://salary-tools-beta.vercel.app/",
       lastModified: new Date(),
     },
     {
-      url: "https://YOURDOMAIN.com/in-hand-salary-calculator",
+      url: "https://salary-tools-beta.vercel.app/in-hand-salary-calculator",
       lastModified: new Date(),
     },
     {
-      url: "https://YOURDOMAIN.com/salary-hike-calculator",
+      url: "https://salary-tools-beta.vercel.app/salary-hike-calculator",
       lastModified: new Date(),
     },
     {
-      url: "https://YOURDOMAIN.com/pf-calculator",
+      url: "https://salary-tools-beta.vercel.app/pf-calculator",
       lastModified: new Date(),
     },
   ];
